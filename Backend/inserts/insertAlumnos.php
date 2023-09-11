874ed5f17cdfcca1a7d06ab43554e288e7d56361
Janusa13 <facundojanusa@gmail.com>
2023-09-11 16:38:21 -0300

    [FEATURE] validacion de datos, pages de registros

--- Backend/inserts/insertAlumnos.php
+++ Backend/inserts/insertAlumnos.php
@@ -5,7 +5,15 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     $nombre=$_POST["nombre"];
     $apellido=$_POST["apellido"];
     $fecha_nac=$_POST["fecha_nac"];
+
     if($alumn_dni && $nombre && $apellido && $fecha_nac != NULL ){
+        $check_query ="SELECT COUNT(*) FROM alumno WHERE alumn_dni = :alumn_dni";
+        $check_stmt = $connect->prepare($check_query);
+        $check_stmt->bindParam(":alumn_dni", $alumn_dni);
+        $check_stmt->execute();
+        $num_rows=$check_stmt->fetchColumn();
+
+    if ($num_rows==0){
         $query="insert into alumno(alumn_dni,nombre,apellido,fecha_nac) 
             values(:alumn_dni,:nombre,:apellido,:fecha_nac)";
         $stmt=$connect->prepare($query);
@@ -14,14 +22,15 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
         $stmt->bindParam(":apellido",$apellido);
         $stmt->bindParam(":fecha_nac",$fecha_nac);
         $stmt->execute();
-        print($nombre);
+        print($nombre . "agregado correctamente.");
         
         
     }else{
-        print("DATOS VACIOS");
+        print("DATOS YA EXISTENTES");
     }
 }else{
-    print("METODO NO VALIDO");
+    print("DATOS VACIOS");
+}
 }
 
 //$queryUpd='update jugadoras set nombre=:nombre, apellido=:apellido, club=:club, edad=:edad where id=:id';
