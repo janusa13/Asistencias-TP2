4de46b24928dadac957b94a68c3bf5640180ead0
Janusa13 <facundojanusa@gmail.com>
2023-09-11 19:34:26 -0300

    [feature] insert materia page

--- Backend/inserts/insertMateria.php
+++ Backend/inserts/insertMateria.php
@@ -1,11 +1,11 @@
 <?php
 require_once("conexion.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $materia_id = $_POST["id"];
+    $materia_id = $_POST["materia_ID"];
     $nombre = $_POST["nombre"];
     $dias = $_POST["dias"];
     if ($nombre && $materia_id && $dias != NULL) {
-        $query = "insert into materia(id,nombre,dias) 
+        $query = "insert into materia(id,nombre,dias, profesor_FK); 
             values(:id,:nombre,:dias)";
         $stmt = $connect->prepare($query);
         $stmt->bindParam("id:", $materia_id);
