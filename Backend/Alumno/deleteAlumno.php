78a5ab15235c4ead0a87069afd04a2bf8ea5c240
DaianF <daianf251@gmail.com>
2023-10-04 11:05:15 -0300

    [FEAT] se agrego delete

---
+++ Backend/Alumno/deleteAlumno.php
@@ -0,0 +1,14 @@
+<?php
+include("..\Conexion\conexion.php");
+$BD = Conexion::connect();
+$alumn_DNI = $_GET["alumn_DNI"];
+$deleteQuery = "DELETE FROM alumno WHERE alumn_DNI= ?";
+$deleteForm = mysqli_prepare($BD, $deleteQuery);
+mysqli_stmt_bind_param($deleteForm, "i", $alumn_DNI);
+mysqli_stmt_execute($deleteForm);
+if ($deleteForm) {
+    header("insertAlumno.php");
+}else{
+    echo"no anduvo";
+}
+?>
