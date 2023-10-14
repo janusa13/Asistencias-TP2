0c8ce01c58ab631d6aa6d4e7db58975a8f8af15f
Janusa13 <facundojanusa@gmail.com>
2023-10-14 13:19:25 -0300

    [fix] borrar alumno

--- Backend/Alumno/deleteAlumno.php
+++ Backend/Alumno/deleteAlumno.php
@@ -2,6 +2,11 @@
 include("..\Conexion\conexion.php");
 $BD = Conexion::connect();
 $alumn_DNI = $_GET["alumn_DNI"];
+$delete_materia_Query = "DELETE FROM alumno_materia WHERE alumno_fk= ?";
+$delete_materia_Form = mysqli_prepare($BD, $delete_materia_Query);
+mysqli_stmt_bind_param($delete_materia_Form, "i", $alumno_fk);
+mysqli_stmt_execute($delete_materia_Form);
+
 $deleteQuery = "DELETE FROM alumno WHERE alumn_DNI= ?";
 $deleteForm = mysqli_prepare($BD, $deleteQuery);
 mysqli_stmt_bind_param($deleteForm, "i", $alumn_DNI);
@@ -13,3 +18,4 @@ if ($deleteForm) {
     echo "no anduvo";
 }
 ?>
+
